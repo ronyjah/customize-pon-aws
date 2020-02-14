b3c75aea23b2aa21e04efbcc1baeb79a49b5b556
Ronaldo Borges <ronaldo.borges@intelbras.com.br>
2020-02-14 11:26:11 -0300

    (feat) Deploy to aws

--- mysite/settings.py
+++ mysite/settings.py
@@ -107,15 +107,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
-
-TIME_ZONE = 'America/Sao_Paulo'
-
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'US/Pacific'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
+
+
 
 
 # Static files (CSS, JavaScript, Images)
